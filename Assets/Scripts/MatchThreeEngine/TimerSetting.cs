112bc91f630d27e84f2bf1df6dee163dd04ea1bc
Finn331 <93345815+Finn331@users.noreply.github.com>
2023-11-08 14:14:14 +0700

    Penambahan timer, Gameover screen dan memindahkan folder asset
    disini gameover screen yang digunakan hanyalah sementara, habis itu gw memindahkan asset tim ke dalam folder baru yang ada di unity yang bernama "Sprites & Assets"

---
+++ Assets/Scripts/MatchThreeEngine/TimerSetting.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class TimerSetting : MonoBehaviour
+{
+    public TextMeshProUGUI TextTimer;
+    public float Waktu = 100; // 01:40 (dalam detik)
+
+    private bool GameAktif = true;
+    public GameObject CanvasKalah;
+
+    private float s;
+
+    private void Update()
+    {
+        if (GameAktif)
+        {
+            s += Time.deltaTime;
+            if (s >= 1)
+            {
+                Waktu--;
+                s = 0;
+            }
+        }
+
+        if (GameAktif && Waktu <= 0)
+        {
+            Debug.Log("Game Kalah");
+            CanvasKalah.SetActive(true);
+            GameAktif = false;
+        }
+
+        SetText();
+    }
+
+    private void SetText()
+    {
+        int Menit = Mathf.FloorToInt(Waktu / 60);
+        int Detik = Mathf.FloorToInt(Waktu % 60);
+
+        // Format Menit dan Detik menjadi teks sesuai dengan kebutuhan (misalnya, menampilkan "01:40").
+        string formattedTime = string.Format("{0:00}:{1:00}", Menit, Detik);
+
+        // Set teks pada komponen TextTimer.
+        TextTimer.text = formattedTime;
+    }
+}
