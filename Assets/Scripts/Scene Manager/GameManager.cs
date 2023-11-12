c27a7d0c0e829401acaf9860fdeab4293758bc5b
Dwi Nandra <dwinandra7@gmail.com>
2023-11-12 10:02:25 +0700

    Penambahan Button di Canvas Kalah Sementara
    Tambahin sedikit button biar kalo Canvas Kalah sementara nya aktif bisa balik lagi ke scene Main Menu. dengan sedikit tambahan Script Game Manager di Scene_2 (Gameplaynya)

---
+++ Assets/Scripts/Scene Manager/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
