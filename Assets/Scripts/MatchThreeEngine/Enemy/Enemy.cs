6b03eba4cbc998e0be5c62dd4d6775bd480b5788
Finn331 <93345815+Finn331@users.noreply.github.com>
2023-12-07 02:19:42 +0700

    Progression Bar *not done yet*
    disini kita menambahkan Progression Bar tetapi belum jadi, karena belum ada sistem coin/point yang kita buat

--- Assets/Scripts/MatchThreeEngine/Enemy/Enemy.cs
+++ Assets/Scripts/MatchThreeEngine/Enemy/Enemy.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public GameObject TimerManager;
     public TimerSetting timerSetting;
+
     public int hp;
     public int attack;
     public int currentHealth;
+    
     private Animator anim;
+
+    public Image progressionBar;
+    public Slider sliderCoba;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +28,9 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
 
-    public void EnemyAttack(/*int attack*/)
+    public void EnemyAttack(/*/int attack/*/)
     {
         Debug.Log("Enemy Attack");
         anim.SetTrigger("enemy_kickT");
@@ -35,12 +41,27 @@ public class Enemy : MonoBehaviour
         Debug.Log("kena serang");
         currentHealth -= dmg;
 
+        if (currentHealth <= 50)
+        {
+            float fillValue = 1;
+            sliderCoba.value = fillValue;
+
+        }
+        if (currentHealth <= 25)
+        {
+            float fillValue = 2;
+            sliderCoba.value = fillValue;
+        }
+
+
         if (currentHealth <= 0)
         {
             //anim enemy nya die
             timerSetting.CanvasMenang.SetActive(true);
             timerSetting.GameAktif = false;
             TimerManager.gameObject.SetActive(false);
+            float fillValue = 3;
+            sliderCoba.value = fillValue;
         }
     }
 }
