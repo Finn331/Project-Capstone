51139179c945ee38cccd5836f741402b3077f88e
Dwi Nandra <dwinandra7@gmail.com>
2023-12-19 18:47:01 +0700

    perbaikan

--- Assets/Scripts/Scene Manager/LevelManager.cs
+++ Assets/Scripts/Scene Manager/LevelManager.cs
@@ -52,7 +52,7 @@ public class LevelManager : MonoBehaviour
     public void RestartLevel()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("Level " + SaveManager.instance.levelSelected);
+        SceneManager.LoadScene("Level " + SaveManager.instance.level);
     }
 
     // UI
